9314fe10d2ea9771b52806628a0f3978d550620a
a.rechkalov <a.rechkalov@2gis.ru>
2016-02-11 18:52:00 +0600

    Добавил возможность указать пользовательскую функцию сравнения ключей

--- 2GIS.NuClear.Utils.Join/Factory.cs
+++ 2GIS.NuClear.Utils.Join/Factory.cs
@@ -20,5 +20,39 @@ namespace NuClear.Utils.Join
             var provider = new MemoryJoinProvider<T1, T2, TOut>(left, right, optimizer, joiner);
             return new MemoryJoinQueryable<TOut>(provider);
         }
+
+        /// <summary>
+        /// С возможностью указать сравнение ключа. Следует учитывать, что сравнение должно совпадать с тем, что реализовано на уровне хранилища, иначе гарантированы проблемы.
+        /// </summary>
+        public static IQueryable<TOut> MemoryJoin<T1, T2, TKey, TOut>(
+            this IQueryable<T1> left,
+            Expression<Func<T1, TKey>> leftKeySelector,
+            IQueryable<T2> right,
+            Expression<Func<T2, TKey>> rightKeySelector,
+            Expression<Func<T1, T2, TOut>> mergeExpression,
+            IComparer<TKey> comparer)
+        {
+            var optimizer = new SimpleQueryOptimizer<T1, T2, TKey>(leftKeySelector, rightKeySelector);
+            var joiner = new Joiner<T1, T2, TKey, TOut>(leftKeySelector.Compile(), rightKeySelector.Compile(), comparer, mergeExpression.Compile());
+            var provider = new MemoryJoinProvider<T1, T2, TOut>(left, right, optimizer, joiner);
+            return new MemoryJoinQueryable<TOut>(provider);
+        }
+
+        /// <summary>
+        /// С возможностью указать сравнение ключа. Следует учитывать, что сравнение должно совпадать с тем, что реализовано на уровне хранилища, иначе гарантированы проблемы.
+        /// </summary>
+        public static IQueryable<TOut> MemoryJoin<T1, T2, TKey, TOut>(
+            this IQueryable<T1> left,
+            Expression<Func<T1, TKey>> leftKeySelector,
+            IQueryable<T2> right,
+            Expression<Func<T2, TKey>> rightKeySelector,
+            Expression<Func<T1, T2, TOut>> mergeExpression,
+            Comparison<TKey> comparison)
+        {
+            var optimizer = new SimpleQueryOptimizer<T1, T2, TKey>(leftKeySelector, rightKeySelector);
+            var joiner = new Joiner<T1, T2, TKey, TOut>(leftKeySelector.Compile(), rightKeySelector.Compile(), Comparer<TKey>.Create(comparison), mergeExpression.Compile());
+            var provider = new MemoryJoinProvider<T1, T2, TOut>(left, right, optimizer, joiner);
+            return new MemoryJoinQueryable<TOut>(provider);
+        }
     }
 }
